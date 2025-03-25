a0f9235c03d3b543934cc2c746e9d49fd4d84071
Aromal P J <aromal.pj@trenser.com>
2025-03-25 17:24:14 +0530

    Build

---
+++ helloworld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int a, int b, int c)
+{
+        int sum = a +b;
+}
+
+
+void main()
+{
+        int* addition = malloc(4);
+
+        printf("Hello World /n");
+        printf("Sum is %d", add(1,2,0));
+}
